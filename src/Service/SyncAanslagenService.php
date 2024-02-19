7d2f46288cb6f116ddd7be466641becad79edffc
Wilco Louwerse <wilco@conduction.nl>
2024-02-19 16:33:49 +0100

    Let's not try to do multiple calls when we can just do one get call

--- src/Service/SyncAanslagenService.php
+++ src/Service/SyncAanslagenService.php
@@ -175,7 +175,13 @@ class SyncAanslagenService
             'belastingjaar-vanaf' => $twoYearsAgo,
         ];
         try {
-            $fetchedAanslagen = $this->callService->getAllResults($source, $endpoint, ['query' => $query], 'result.instance.rows?');
+            $this->stopwatch->start('fetchAanslagen-getAllResults', 'open-belasting-bundle');
+            $response = $this->callService->call($source, $endpoint, 'GET', ['query' => $query]);
+            $fetchedAanslagen = $this->callService->decodeResponse($source, $response);
+            
+            // In case of pagination we would need getAllResults instead of call:
+//            $fetchedAanslagen = $this->callService->getAllResults($source, $endpoint, ['query' => $query], 'result.instance.rows?');
+            $this->stopwatch->stop('fetchAanslagen-getAllResults');
         } catch (Exception $e) {
             $this->logger->error("Failed to fetch: {$e->getMessage()}");
 
